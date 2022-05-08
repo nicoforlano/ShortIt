7f41b325e517ed516ebf2ff547bc50d344d0b7b6
Nicolas Forlano <nicoforlanop@gmail.com>
2022-05-08 18:04:56 -0300

    Initial Commit

---
+++ src/main/java/com/nicof/shortit/ShortitApplication.java
@@ -0,0 +1,13 @@
+package com.nicof.shortit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicof/shortit/ShortitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicof.shortit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
